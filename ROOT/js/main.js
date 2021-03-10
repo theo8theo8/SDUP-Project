4004a0b7a54366bd56b8e0d42c2a8b39f74f0351
Theo Meier Ström <meier.strom.theo@gmail.com>
2021-03-10 12:23:15 +0100

    Removed unnecessary code

--- ROOT/js/main.js
+++ ROOT/js/main.js
@@ -30,39 +30,6 @@ function update_view() {
 $(document).ready(function() {
     loadMgrEditStock();
     loadCategoryDropdown();
-
-    let items = document.querySelectorAll("#twoPtable");
-    var dragItem;
-    var container = document.getElementById("bordskarta");
-    var order = document.getElementById("order");
-    var active = false;
-    var currentX;
-    var currentY;
-    var initialX;
-    var initialY;
-    var xOffset = 0;
-    var yOffset = 0;
-    var contSize = 0;
-    var itemSize = 0;
-    var nextNum = 1;
-    var currentTableID = 0;
-    var menuInfoCont = document.getElementById("menuInfoCont");
-    var lastMenu = 'all';
-    var currentFiltering = 'none';
-    var alcoPercent = 0;
-    
-    container.addEventListener("touchstart", dragStart, false);
-    container.addEventListener("touchend", dragEnd, false);
-    container.addEventListener("touchmove", drag, false);
-    
-    container.addEventListener("mousedown", dragStart, false);
-    container.addEventListener("mouseup", dragEnd, false);
-    container.addEventListener("mousemove", drag, false);
-    
-    
-    loadAllTables();
-    showMenu();
-
     update_view();
     $("#login-view").css("display","block");
 })
